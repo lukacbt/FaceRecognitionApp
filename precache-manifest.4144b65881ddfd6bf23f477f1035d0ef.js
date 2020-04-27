e5c8ed8af1ad9b9bac1fefa904e2065276cc6435
Luka Kukina <lukscbt@gmail.com>
2020-04-27 12:24:25 +0200

    Updates

---
+++ precache-manifest.4144b65881ddfd6bf23f477f1035d0ef.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b09fc2ff89b80f75ee96a8ab10ac0f2",
+    "url": "/FaceRecognitionApp/index.html"
+  },
+  {
+    "revision": "5c47662d2d13411bf9b6",
+    "url": "/FaceRecognitionApp/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "6d1890ed280f93a43598",
+    "url": "/FaceRecognitionApp/static/css/main.20ebae69.chunk.css"
+  },
+  {
+    "revision": "5c47662d2d13411bf9b6",
+    "url": "/FaceRecognitionApp/static/js/2.4a05ae11.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/FaceRecognitionApp/static/js/2.4a05ae11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d1890ed280f93a43598",
+    "url": "/FaceRecognitionApp/static/js/main.d471867f.chunk.js"
+  },
+  {
+    "revision": "eb1149826d03b42f5653",
+    "url": "/FaceRecognitionApp/static/js/runtime-main.382f386a.js"
+  },
+  {
+    "revision": "c1a574687497d262f82aae6fa19c9576",
+    "url": "/FaceRecognitionApp/static/media/brain.c1a57468.png"
+  }
+]);
